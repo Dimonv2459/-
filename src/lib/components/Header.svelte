ffb99ba1aa26936d7a8f75dd76d1ea58f8fa4dcf
stud <stud@it233-2.almetpt.local>
2025-04-17 18:40:30 +0300

    Новый коммит

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import {Nav,Navbar,NavbarBrand, NavItem, NavLink} from '$sveltestrap/sveltestrap'
+</script>
+
+<Navbar>
+      <NavbarBrand>Logo</NavbarBrand>
+
+      <Nav>
+          <NavItem></NavLink href="/">Главная</NavLink></NavItem>
+          <NavItem></NavLink href="/"about">О нас</NavLink></NavItem>
+      </Nav>
+</Navbar>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Header from "$lib/components/Header.svelte"
+import { Container, Styles } from "$sve"
+    let {children} = $props()
+</script>
+
+<Styles/>
+<Container>
+    <Header/>
+    {@render children()}
+</Container>
